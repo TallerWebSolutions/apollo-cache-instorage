9c15a6b0aa8f2cb04b65bf01a1a84fbc83fd91ef
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2018-10-21 22:15:27 -0200

    Added document transform helpers

---
+++ src/transform.js
@@ -0,0 +1,56 @@
+import { checkDocument, cloneDeep } from 'apollo-utilities'
+
+const PERSIST_FIELD = {
+  kind: 'Field',
+  name: {
+    kind: 'Name',
+    value: '__persist'
+  }
+}
+
+const addPersistFieldToSelectionSet = (selectionSet, isRoot = false) => {
+  if (selectionSet.selections) {
+    if (!isRoot) {
+      const alreadyHasThisField = selectionSet.selections.some(selection => {
+        return (
+          selection.kind === 'Field' && selection.name.value === '__typename'
+        )
+      })
+
+      if (!alreadyHasThisField) {
+        selectionSet.selections.push(PERSIST_FIELD)
+      }
+    }
+
+    selectionSet.selections.forEach(selection => {
+      // Must not add __typename if we're inside an introspection query
+      if (selection.kind === 'Field') {
+        if (
+          selection.name.value.lastIndexOf('__', 0) !== 0 &&
+          selection.selectionSet
+        ) {
+          addPersistFieldToSelectionSet(selection.selectionSet)
+        }
+      }
+      else if (selection.kind === 'InlineFragment') {
+        if (selection.selectionSet) {
+          addPersistFieldToSelectionSet(selection.selectionSet)
+        }
+      }
+    })
+  }
+}
+
+const addPersistFieldToDocument = doc => {
+  checkDocument(doc)
+  const docClone = cloneDeep(doc)
+
+  docClone.definitions.forEach(definition => {
+    const isRoot = definition.kind === 'OperationDefinition'
+    addPersistFieldToSelectionSet(definition.selectionSet, isRoot)
+  })
+
+  return docClone
+}
+
+export { addPersistFieldToDocument }
